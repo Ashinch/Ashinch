1859f0337444df437991f3ac1e08fa34c7695f15
Ash <Glaxyinfinite@outlook.com>
2024-11-14 18:55:19 +0800

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -29,7 +29,7 @@ def fetch_blog_entries():
         {
             "title": entry["title"],
             "url": entry["link"].split("#")[0],
-            "published": format_time(entry["published"].split("T")[0]),
+            "published": format_time(entry["pubDate"].split("T")[0]),
         }
         for entry in entries
     ]
